ebfee6b228466c978cf1d59cf5b9b104dc15ccd8
Nasta <arrgohn.work@gmail.com>
2022-01-05 12:29:54 +0300

    hw10 changed json library

--- hw10_program_optimization/go.mod
+++ hw10_program_optimization/go.mod
@@ -3,7 +3,8 @@ module github.com/Arrgohn/otus_hw/hw10_program_optimization
 go 1.16
 
 require (
-	github.com/json-iterator/go v1.1.12
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	github.com/valyala/fastjson v1.6.3
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,33 +2,63 @@ package hw10programoptimization
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"strings"
 
-	jsoniter "github.com/json-iterator/go"
+	"github.com/valyala/fastjson"
 )
-
-type User struct {
-	Email string
-}
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	u, err := getUsers(r)
+	if err != nil {
+		return nil, fmt.Errorf("get users error: %w", err)
+	}
+	return countDomains(u, domain)
+}
+
+func getUsers(r io.Reader) (result []string, err error) {
 	scanner := bufio.NewScanner(r)
-	result := make(DomainStat)
-	user := User{}
-	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	var sc fastjson.Scanner
+	var email string
 
 	for scanner.Scan() {
-		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
-			return nil, err
-		}
+		bytes := scanner.Bytes()
 
-		if strings.Contains(user.Email, "."+domain) {
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
+		sc.InitBytes(bytes)
+		for sc.Next() {
+			if sc.Value().Get("Email") != nil {
+				email = sc.Value().Get("Email").String()
+				result = append(result, email[1:len(email)-1])
+			}
 		}
 	}
 
+	if len(result) == 0 {
+		return []string{}, errors.New("no emails found")
+	}
+	return result, err
+}
+
+func countDomains(emails []string, domain string) (DomainStat, error) {
+	result := make(DomainStat)
+
+	var p []string
+	var userDomain string
+	for _, email := range emails {
+		p = strings.Split(email, ".")
+		if len(p) < 2 {
+			continue
+		}
+
+		userDomain = strings.ToLower(p[len(p)-1])
+		if userDomain == domain {
+			address := strings.ToLower(strings.Split(email, "@")[1])
+			result[address]++
+		}
+	}
 	return result, nil
 }
